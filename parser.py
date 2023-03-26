57c3dd99caf607f699ca37ce9a20bc34b81fd60c
p4sh4bsc <testsearc@gmail.com>
2023-03-26 22:14:24 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -9,22 +9,21 @@ from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime
 import csv
 from selenium.webdriver.common.keys import Keys 
-
+import argparse
 
 
 options = webdriver.ChromeOptions()
 options.headless=True
-#options.add_argument("--no-sandbox")
+options.add_argument("--no-sandbox")
 
-id_of_cards = []
-name_of_cards = []
-price_of_cards = []
-count_of_stars = []
-delivery_dates = []
+arg = argparse.ArgumentParser(description='Сортировка по ценам и количеству звёзд')
+arg.add_argument('-p', '--price', type=bool, const=True, default=False, nargs='?', help='Сортировка по убыванию цен')
+arg.add_argument('-s', '--stars', type=bool, const=True, default=False, nargs='?', help='Сортировка по убыванию отзывов')
+param = arg.parse_args()
 
 def get_website(item, page):
     
-    url = 'https://www.wildberries.ru/catalog/0/search.aspx?page='+page+'&sort=popular&search='+item
+    url = 'https://www.wildberries.ru/catalog/0/search.aspx?page='+str(page)+'&sort=popular&search='+item
     print(url)
     
     
@@ -47,6 +46,22 @@ def get_website(item, page):
         driver.quit()
         
 def get_cards():
+    global id_of_cards
+    global name_of_cards
+    global price_of_cards
+    global count_of_stars
+    global delivery_dates
+    global list_of_items
+    
+    id_of_cards = []
+    name_of_cards = []
+    price_of_cards = []
+    count_of_stars = []
+    delivery_dates = []
+    
+    list_of_items = []
+    
+    
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     with open("index.html", encoding="utf-8") as file:
         src = file.read()
@@ -56,17 +71,23 @@ def get_cards():
     cards = all_cards.find_all("div", class_ = "product-card j-card-item")
     
     for item in cards:
-        
+        item_info = []
         ############# GET ID OF CARD #############
-        id_of_card = item.get("data-nm-id")
-        id_of_cards.append(id_of_card)
+        try:
+            id_of_card = item.get("data-nm-id")
+        except:
+            id_of_card = "Не указано"
+        item_info.append(id_of_card)
         ##########################################
         
         
         ############# GET NAME OF CARD #############
         name_of_card = item.find("span", class_="goods-name")
-        name = (str(name_of_card.text)).replace("/","").strip()
-        name_of_cards.append(name)
+        try:
+            name = (str(name_of_card.text)).replace("/","").strip()
+        except:
+            name = "Не указано"
+        item_info.append(name)
         ##########################################
         
         
@@ -74,24 +95,37 @@ def get_cards():
         class_of_price = item.find("span", class_ = "price__lower-price")
         if class_of_price == None:
             class_of_price = item.find("ins", class_ = "price__lower-price")
-        price = str(class_of_price.text)
-        price = ''.join(price.split())
-        price = price[0:-1]+"руб."
-        price_of_cards.append(price)
+        try:
+            price = str(class_of_price.text)
+            price = ''.join(price.split())
+            price = price[0:-1]
+            price = int(price)
+        except:
+            price = "Не указана"
+        item_info.append(price)
         ##########################################
         
         
         ############# GET STARS OF CARD #############
         class_of_stars = item.find("span", class_ = "product-card__count")
-        stars = str(class_of_stars.text)
-        stars = ''.join(stars.split())
-        count_of_stars.append(stars)
+        try:
+            stars = str(class_of_stars.text)
+            stars = ''.join(stars.split())
+            stars = int(stars)
+        except:
+            stars = "Не указано"
+        item_info.append(stars)   
         ##########################################
         
         ############# GET DELIVERY-DATE OF CARD #############
         class_of_delivery_date = item.find("b", class_ = "product-card__delivery-date")
-        delivery_date = (str(class_of_delivery_date.text)).strip()
-        delivery_dates.append(delivery_date)
+        
+        try:
+            delivery_date = (str(class_of_delivery_date.text)).strip()
+        except:
+            delivery_date = "Не указана"
+        item_info.append(delivery_date)
+        list_of_items.append(item_info)
 
 def create_csv():
     global date_for_excel
@@ -102,28 +136,28 @@ def create_csv():
         writer.writerow([" ID", "Название товара", "Цена товара", "Число отзывов", "Дата доставки"])
         
 def load_data_to_csv():
-    for i in range(len(id_of_cards)):
-        list_of_data = []
-        id = id_of_cards[i]
-        name = name_of_cards[i]
-        price = price_of_cards[i]
-        stars = count_of_stars[i]
-        date_d = delivery_dates[i]
+    if param.price and param.stars:
+        print("Укажите только один тип сортировки")
+    elif param.price:
+        list_of_items.sort(key=lambda x: x[2], reverse=True)
+        print("sort by price")
+    elif param.stars:
+        list_of_items.sort(key=lambda x: x[3], reverse=True)
+        print("sort by stars")
+    
+    for i in range(len(list_of_items)):
         
-        list_of_data.append(id)
-        list_of_data.append(name)
-        list_of_data.append(price)
-        list_of_data.append(stars)
-        list_of_data.append(date_d)
         with open(date_for_excel+".csv", "a", encoding="utf-8", newline="") as f:
             writer = csv.writer(f, delimiter=";")
-            writer.writerow(list_of_data)
+            writer.writerow(list_of_items[i])
 
 if __name__ == '__main__':
     tprint("Parser Wildberries")
     name_of_item = str(input("Введите название товара: "))
-    #count_of_pages = str(input("Введите количество страниц для парсинга: "))
-    get_website(name_of_item, "1")
-    get_cards()
+    count_of_pages = int(input("Введите количество страниц для парсинга: "))
+    
     create_csv()
-    load_data_to_csv()
+    for i in range(count_of_pages):
+        get_website(name_of_item, count_of_pages)
+        get_cards()
+        load_data_to_csv()
